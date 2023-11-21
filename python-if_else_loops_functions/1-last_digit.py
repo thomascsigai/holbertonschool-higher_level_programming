8fe16c243adac906ee5896c6161f1a7129b8e302
Thomas Csigai <thomascsigai1@gmail.com>
2023-11-21 14:37:13 +0100

    Handling the case where number is negative

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -2,7 +2,7 @@
 import random
 number = random.randint(-10000, 10000)
 
-lastDigit = abs(number) % 10
+lastDigit = number % (int(number/abs(number)) * 10)
 
 print("Last digit of", number, "is", lastDigit, end="")
 
