f3bddc114b0a4109612c8fc2f30f4725cb7d0d32
Thomas Csigai <thomascsigai1@gmail.com>
2023-11-24 17:16:47 +0100

    Changing doc + Changing raise error condition

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -3,11 +3,15 @@
 
 
 class Square:
-    '''Defines a square'''
+    '''
+    Defines a square
+    
+        size: the size of the square
+    '''
 
     def __init__(self, size=0):
         '''Square Initialisation'''
-        if type(size) is int:
+        if type(size) is not int:
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
