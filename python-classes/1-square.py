78eb9b8e271bace3539b4ba0105acb598bdac19d
Thomas Csigai <thomascsigai1@gmail.com>
2023-11-24 14:56:42 +0100

    Adding task 1 program

---
+++ python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+'''This module contains the definition of the Square Class'''
+
+
+class Square:
+    '''Defines a square'''
+    def __init__(self, size):
+        '''Square Initialisation'''
+        self.__size = size
