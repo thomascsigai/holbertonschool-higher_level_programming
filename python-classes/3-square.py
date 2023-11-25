8c0a565c8a880a339942700954afbfbbb1bd2658
Thomas Csigai <thomascsigai1@gmail.com>
2023-11-25 21:22:57 +0100

    Adding task 3 program

---
+++ python-classes/3-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+'''
+This module contains the definition of the Square Class
+'''
+
+
+class Square:
+    '''
+    Defines a square
+
+        size: the size of the square
+    '''
+
+    def __init__(self, size=0):
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        return self.__size * self.__size
