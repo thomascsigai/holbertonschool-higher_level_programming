e016833f2453b13247365b994f9add5b86a90059
Thomas Csigai <thomascsigai1@gmail.com>
2023-11-25 21:39:34 +0100

    Removing test code + Validate pycodestyle

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -17,7 +17,7 @@ class Square:
     @property
     def size(self):
         return self.__size
-    
+
     @size.setter
     def size(self, size):
         if type(size) is not int:
@@ -29,15 +29,3 @@ class Square:
 
     def area(self):
         return self.__size * self.__size
-    
-my_square = Square(89)
-print("Area: {} for size: {}".format(my_square.area(), my_square.size))
-
-my_square.size = 3
-print("Area: {} for size: {}".format(my_square.area(), my_square.size))
-
-try:
-    my_square.size = "5 feet"
-    print("Area: {} for size: {}".format(my_square.area(), my_square.size))
-except Exception as e:
-    print(e)
